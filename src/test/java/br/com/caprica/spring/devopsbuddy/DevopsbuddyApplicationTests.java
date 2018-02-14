0c843739cc6412c8be6a79635534ac61fd2ca5b9
Paola Froes <paola.froes@gmail.com>
2018-02-13 22:55:51 -0200

    Added i18n capability to the app

--- src/test/java/br/com/caprica/spring/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/br/com/caprica/spring/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,28 @@
 package br.com.caprica.spring.devopsbuddy;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.caprica.spring.devopsbuddy.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18NService i18nService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() {
+		String expectedResult = "Hello, Spring Boot World!";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessageSource(messageId);
+		Assert.assertEquals("No match!", expectedResult, actual);
 	}
 
 }
