d036cd95a6302e2a7a44dfaf24885b4c3c29feb8
Paola Froes <paola.froes@gmail.com>
2018-02-14 09:49:48 -0200

    Adding logging capabilities

--- src/main/java/br/com/caprica/spring/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/br/com/caprica/spring/devopsbuddy/web/i18n/I18NService.java
@@ -7,13 +7,20 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class I18NService {
+	
+	/** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
 	
 	@Autowired
 	private MessageSource messageSource;
 	
 	public String getMessageSource(String messageId) {
+		LOG.info("Retornando o texto i18n para o id: "+messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return messageSource.getMessage(messageId, null, locale);
 	}
