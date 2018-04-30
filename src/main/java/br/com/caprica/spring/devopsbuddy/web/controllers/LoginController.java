855237eadaa5cded30a058960ee55ce97e932866
gitpfroes <paola.froes@gmail.com>
2018-04-30 20:10:16 -0300

    Security part 1

--- src/main/java/br/com/caprica/spring/devopsbuddy/web/controllers/LoginController.java
+++ src/main/java/br/com/caprica/spring/devopsbuddy/web/controllers/LoginController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	public static final String LOGIN_VIEW_NAME = "user/login";
-	
-	@RequestMapping("/login")
-	public String login() {
-		return LOGIN_VIEW_NAME;
-	}
+
+    /** The login view name */
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login() {
+        return LOGIN_VIEW_NAME;
+    }
 }
